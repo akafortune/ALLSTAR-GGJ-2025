2e55525726ab1017ff542428984c353fbd83e494
Connor McDonald <117039387+akafortune@users.noreply.github.com>
2025-01-25 14:16:02 -0500

    basic respawn

---
+++ Assets/Respawn_Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn_Manager : MonoBehaviour
+{
+    public static Transform currRespawnPos;
+
+    public Transform[] respawnPosArr;
+    public int respawnIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
