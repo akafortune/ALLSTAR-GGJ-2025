439cc53fab19474b57ab36e920133072eb2ddd3e
AidanKleine <aidankleine@outlook.com>
2025-01-25 10:19:55 -0500

    Camera cooldown
    - added code for camera cooldown; disables camera move trigger for short period. Adjustable
    - switched camera trigger to a square obj to make visible in game view for testing

--- Assets/Scripts/Player/Camera_Trigger.cs
+++ Assets/Scripts/Player/Camera_Trigger.cs
@@ -5,12 +5,37 @@ using UnityEngine;
 public class Camera_Trigger : MonoBehaviour
 {
     public Camera mainCam;
+    public Collider2D myCol;
+    public int timerBound;
+    int timer = 0;
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag.Equals("Player"))
         {
             mainCam.SendMessage("NextCam");
+            TriggerCooldown();
+        }
+    }
+
+    void TriggerCooldown()  //starts collider cooldown
+    {
+        if (myCol.enabled)
+        {
+            timer = 0;
+            myCol.enabled = false;
+        }
+    }
+
+    void Update() //controls timer for collider cooldown
+    {
+        if (timer < timerBound && !myCol.enabled)
+        {
+            timer++;
+        }
+        else if (!myCol.enabled)
+        {
+            myCol.enabled = true;
         }
     }
 }
