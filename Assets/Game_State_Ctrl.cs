fc3273c830ffdff5afee5d2aa07454d18edcce55
AidanKleine <aidankleine@outlook.com>
2025-01-25 14:13:29 -0500

    Dialogue done

--- Assets/Game_State_Ctrl.cs
+++ Assets/Game_State_Ctrl.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+
+//https://medium.com/@jonbednez/how-to-create-typewriter-effect-in-ui-with-unity-a-step-by-step-tutorial-bcd6d33d86d4
 
 public enum GameState { Play, Cutscene};
 
@@ -18,16 +22,28 @@ public class Game_State_Ctrl : MonoBehaviour
     List<string> sceneB;
     List<string> sceneC;
 
+    public TMP_Text textObj;
+
+    float typeSpd = 0.1f;
+    float timePerChar = 2f;
+    float timer = 0;
+    int charIndex = 0;
+
+    List<string> curScene;
+
+
     void Start()
     {
         curState = GameState.Cutscene;
 
         diaKey = KeyCode.LeftShift;
 
-        sceneA = new List<string> { "first line" , "second line"};
+        sceneA = new List<string> { "first line" , "second line", "third line", ""};
         sceneB = new List<string>();
         sceneC = new List<string>();
         allScenes = new List<List<string>> { sceneA, sceneB, sceneC };
+
+        curScene = allScenes[sceneNum];
     }
 
     void Update()
@@ -40,20 +56,51 @@ public class Game_State_Ctrl : MonoBehaviour
         switch (curState)
         {
             case GameState.Cutscene:
-                RunDialogue();
+                TypeControl();
                 break;
             default: //Play
                 break;
         }
     }
 
-    void RunDialogue()
+    void TypeControl() //Typewriter effect
     {
-        if (Input.GetKeyDown(diaKey))
+        curScene = allScenes[sceneNum];
+        RunDialogue();
+        if (timer < timePerChar)
         {
-            List<string> curScene = allScenes[sceneNum];
-            Debug.Log(curScene[diaNum]);  //placeholder
-            diaNum++;
+            timer += typeSpd;
+        }
+        else if (charIndex < curScene[diaNum].Length)
+        {
+            charIndex++;
+            timer = 0f;
         }
     }
+
+    void RunDialogue() //Display and input
+    {
+        string displDia = curScene[diaNum].Substring(0, charIndex);
+        textObj.text = displDia;
+
+        if (diaNum < curScene.Count - 1)
+        {
+            if (Input.GetKeyDown(diaKey) && displDia.Length == curScene[diaNum].Length)
+            {
+                diaNum++;
+                charIndex = 0;
+                timer = 0;
+            }
+            else if (Input.GetKeyDown(diaKey)) //skip to end of line
+            {
+                charIndex = curScene[diaNum].Length;
+                timer = 0;
+            }
+        }
+        else
+        {
+            Debug.Log("End Scene");
+        }
+    }
+
 }
