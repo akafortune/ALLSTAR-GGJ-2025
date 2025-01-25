ab342a81d7f39d504686c8cabc7f22da4494cc1b
jjscherer <82835446+jjscherer@users.noreply.github.com>
2025-01-25 11:50:22 -0500

    Add basic functioning audio manager
    Adjusted AudioManager calls. Included note at top of file. Added SFX and MUSIC enums for calls.

--- Assets/Scripts/AudioManagement/AudioManager.cs
+++ Assets/Scripts/AudioManagement/AudioManager.cs
@@ -1,45 +1,109 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Note: use "using static AudioManager.SFX" to more easily access SFX enum
+ *       "using static AudioManager.Music" to more easily access Music enum
+ *       
+ *       Perhaps use "using static AudioManager"
+ * 
+ */
+
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager Instance;
+    public static AudioManager Audio;
 
+    #region AudioSources
     [SerializeField] private AudioSource _music;
     private bool _musicPlaying = false;
 
+    [SerializeField] private AudioSource _bubblePop;
+    [SerializeField] private AudioSource _bubbleEntry;
+    [SerializeField] private AudioSource _bubbleExit;
+    [SerializeField] private AudioSource _phone;
+    private bool _phonePlaying = false;
+    #endregion
+
+    #region Awake
     private void Awake()
     {
         //  = GetComponent<AudioSource>();
 
-        if (Instance != null)
+        if (Audio != null)
             Destroy(gameObject);
         else
-            Instance = this;
+            Audio = this;
 
         DontDestroyOnLoad(gameObject);
 
         Debug.Log("Audio Manager awake...");
     }
+    #endregion
 
-    /*public void PlayLevelMusic()
+    #region MusicCalls
+    public void StopMusic()
     {
-        PlayMusic();
-    }*/
+        _music.Stop();
+        _musicPlaying = false;
+    }
 
-    public void PlayMusic()
+    public void PlayMusic(MUSIC music)
     {
+        switch(music)
+        {
+            case MUSIC.LEVEL:
+                _music.Play();
+                _musicPlaying = true;
+                if (!_musicPlaying)
+                {
+                    _music.Play();
+                    _musicPlaying = true;
+                }
+                else
+                {
+                    _music.Stop();
+                    _musicPlaying = false;
+                }
+                break;
+        }
         if (!_musicPlaying)
         {
             _music.Play();
             _musicPlaying = true;
         }
     }
+    #endregion
 
-    public void StopMusic()
+    #region SFX Calls
+    public void PlaySFX(SFX sfx)
     {
-        _music.Stop();
-        _musicPlaying = false;
+        switch (sfx) {
+            case SFX.BUBBLE_POP:
+                _bubblePop.Play();
+                break;
+            case SFX.BUBBLE_ENTRY:
+                _bubbleEntry.Play();
+                break;
+            case SFX.BUBBLE_EXIT:
+                _bubbleExit.Play();
+                break;
+            case SFX.PHONE_RING:
+                if (!_phonePlaying)
+                {
+                    _phone.Play();
+                    _phonePlaying = true;
+                } else
+                {
+                    _phone.Stop();
+                    _phonePlaying = false;
+                }
+                break;
+        }
     }
+    #endregion
+    
+    public enum SFX { PHONE_RING, BUBBLE_ENTRY, BUBBLE_EXIT, BUBBLE_POP }
+    public enum MUSIC { LEVEL }
 }
