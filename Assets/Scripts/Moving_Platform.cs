e0879fdd9aef0f29254f9caa0464d892d7d90fed
AidanKleine <aidankleine@outlook.com>
2025-01-25 20:36:33 -0500

    moving platform script

---
+++ Assets/Scripts/Moving_Platform.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_Platform : MonoBehaviour
+{
+    public Transform pt1;
+    public Transform pt2;
+
+    bool goingToPt1;
+
+    void Update()
+    {
+
+    }
+
+}
